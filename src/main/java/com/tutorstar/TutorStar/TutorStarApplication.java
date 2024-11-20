0193b38e6ec0b46d5c2b34a94a2ea03377a0c41a
stars1lver <diego@somosziro.com>
2024-11-20 10:25:02 -0500

    prueba

---
+++ src/main/java/com/tutorstar/TutorStar/TutorStarApplication.java
@@ -0,0 +1,13 @@
+package com.tutorstar.TutorStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorstar/TutorStar/TutorStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorstar.TutorStar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
